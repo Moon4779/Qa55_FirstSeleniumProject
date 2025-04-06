1d1fb8292e855be5a8047a08666cbd6810dc9c07
tetiana <dzhobolda23@gmail.com>
2025-04-06 15:27:58 +0200

    HW-April06.04

--- src/test/java/com/ait/qa55/HomeWorkFirst.java
+++ src/test/java/com/ait/qa55/HomeWorkFirst.java
@@ -1,6 +1,8 @@
 package com.ait.qa55;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement; // <-- Важно!
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -19,6 +21,27 @@ public class HomeWorkFirst {
     public void openWebsiteTest() {
         System.out.println("Website opened successfully!");
     }
+
+    @Test
+    public void findElementsTest() {
+
+        WebElement element1 = driver.findElement(By.id("dialog-notifications-success"));
+        WebElement element2 = driver.findElement(By.id("dialog-notifications-error"));
+        WebElement element3 = driver.findElement(By.id("bar-notification"));
+        WebElement element4 = driver.findElement(By.id("flyout-cart"));
+        WebElement element5 = driver.findElement(By.id("mob-menu-button"));
+
+
+        WebElement element6 = driver.findElement(By.className("master-wrapper-content"));
+        WebElement element7 = driver.findElement(By.className("header"));
+        WebElement element8 = driver.findElement(By.className("header-logo"));
+        WebElement element9 = driver.findElement(By.className("header-links-wrapper"));
+        WebElement element10 = driver.findElement(By.className("header-links"));
+
+
+        System.out.println("ten elements was found !");
+    }
+
 
     @AfterMethod(enabled = false)
     public void tearDown() {
